67cab8359d3b335b235840f0575cdd8fadfaa5dd
kyle_coffey <kyle_coffey@affirmednetworks.com>
2020-07-27 15:40:52 -0500

    Keep output in return value

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use chrono::Utc;
 use crossbeam_channel::Sender;
-use error_chain::bail;
 use stateful::run_with_status;
-use std::{error::Error, path::Path, process::Command};
+use std::{error::Error, path::Path, process::{Output, Command}};
 
 pub fn cd_and_run<S, P>(
     command: S,
@@ -11,7 +10,7 @@ pub fn cd_and_run<S, P>(
     done_message_sender: &Sender<String>,
     start_sender: &Sender<()>,
     done_sender: &Sender<()>,
-) -> Result<(), Box<dyn Error>>
+) -> Result<Output, Box<dyn Error>>
 where
     S: AsRef<str>,
     P: AsRef<Path>,
@@ -36,14 +35,13 @@ where
 pub fn cd_and_run_no_spinner<S: AsRef<str>, P: AsRef<Path>>(
     command: S,
     chdir: P,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<Output, Box<dyn Error>> {
     let command = command.as_ref();
     let command_parts: Vec<_> = command.split_whitespace().collect();
 
     let mut cmd_to_run = Command::new(command_parts[0]);
     cmd_to_run.args(&command_parts[1..]).current_dir(chdir);
-    run_command_no_spinner(cmd_to_run, command)?;
-    Ok(())
+    Ok(run_command_no_spinner(cmd_to_run, command)?)
 }
 
 pub fn run<S>(
@@ -89,41 +87,35 @@ pub fn run_command<S: AsRef<str>>(
     done_message_sender: &Sender<String>,
     start_sender: &Sender<()>,
     done_sender: &Sender<()>,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<Output, Box<dyn Error>> {
     println!(">> {}", command_str.as_ref());
     let start_time = Utc::now();
     message_sender.send(command_str.as_ref().to_string())?;
     done_message_sender.send(command_str.as_ref().to_string())?;
     start_sender.send(())?;
     let result = run_with_status(command, done_sender, true)?;
-    if !result.status.success() {
-        bail!(format!("Subcommand '{}' failed!", command_str.as_ref()));
-    }
     let time_elapsed = Utc::now() - start_time;
     println!(
         "\x1b[2K\x1b[G> Time elapsed: {:?}",
         time_elapsed.to_std().unwrap()
     );
-    Ok(())
+    Ok(result)
 }
 
 pub fn run_command_no_spinner<S: AsRef<str>>(
     mut command: Command,
     command_str: S,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<Output, Box<dyn Error>> {
     println!(">> {}", command_str.as_ref());
     let start_time = Utc::now();
-    let mut child = command.spawn()?;
-    let result = child.wait()?;
-    if !result.success() {
-        bail!(format!("Subcommand '{}' failed!", command_str.as_ref()));
-    }
+    let child = command.spawn()?;
+    let result = child.wait_with_output()?;
     let time_elapsed = Utc::now() - start_time;
     println!(
         "\x1b[2K\x1b[G> Time elapsed: {:?}",
         time_elapsed.to_std().unwrap()
     );
-    Ok(())
+    Ok(result)
 }
 
 #[cfg(test)]
