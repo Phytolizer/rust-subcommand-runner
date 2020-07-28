2a7d1f5c0f3a645b4430fb51a2af21c00ff2439a
kyle_coffey <kyle_coffey@affirmednetworks.com>
2020-07-28 16:01:37 -0500

    Version 2.0, simpler API

--- src/lib.rs
+++ src/lib.rs
@@ -1,123 +1,144 @@
-use chrono::Utc;
 use crossbeam_channel::Sender;
-use stateful::run_with_status;
+use io::Write;
+use parking_lot::Mutex;
 use std::{
     error::Error,
+    io::{self, BufRead, BufReader, Read},
     path::Path,
     process::{Command, Output},
+    sync::Arc,
+    thread,
 };
 
-pub fn cd_and_run<S, P>(
-    command: S,
-    chdir: P,
-    message_sender: &Sender<String>,
-    done_message_sender: &Sender<String>,
-    start_sender: &Sender<()>,
-    done_sender: &Sender<()>,
+pub struct SpinnerVars<'sp, S>
+where
+    S: AsRef<str>,
+{
+    status_message_sender: &'sp Sender<String>,
+    status_message_override: Option<S>,
+    done_message_sender: &'sp Sender<String>,
+    done_message_override: Option<S>,
+    activate_sender: &'sp Sender<()>,
+    deactivate_sender: &'sp Sender<()>,
+}
+
+impl<'sp, S> SpinnerVars<'sp, S>
+where
+    S: AsRef<str>,
+{
+    pub fn new(
+        status_message_sender: &'sp Sender<String>,
+        status_message_override: Option<S>,
+        done_message_sender: &'sp Sender<String>,
+        done_message_override: Option<S>,
+        activate_sender: &'sp Sender<()>,
+        deactivate_sender: &'sp Sender<()>,
+    ) -> Self {
+        Self {
+            status_message_sender,
+            status_message_override,
+            done_message_sender,
+            done_message_override,
+            activate_sender,
+            deactivate_sender,
+        }
+    }
+}
+
+fn child_stream_to_lines<R>(stream: R) -> Arc<Mutex<Vec<u8>>>
+where
+    R: Read + Send + 'static,
+{
+    let out = Arc::new(Mutex::new(vec![]));
+    let vec = out.clone();
+    let mut reader = BufReader::new(stream);
+    thread::Builder::new()
+        .name("child_stream_to_vec".into())
+        .spawn(move || loop {
+            let mut buf = Vec::with_capacity(1);
+            match reader.read_until(b'\n', &mut buf) {
+                Err(e) => {
+                    eprintln!("line {}: Error reading from stream: {}", line!(), e);
+                    break;
+                }
+                Ok(got) => match got {
+                    0 => break,
+                    _ => *vec.lock() = buf,
+                },
+            }
+        })
+        .unwrap_or_else(|_| panic!("{}}} Could not spawn thread", line!()));
+    out
+}
+
+pub fn run<S, P>(
+    command_name: S,
+    spinner_vars: Option<SpinnerVars<S>>,
+    command: Option<Command>,
+    show_output: bool,
+    cwd: Option<P>,
 ) -> Result<Output, Box<dyn Error>>
 where
     S: AsRef<str>,
     P: AsRef<Path>,
 {
-    let command = command.as_ref();
-    let command_parts: Vec<_> = command.split_whitespace().collect();
+    let command_name = command_name.as_ref();
+    let command_and_args: Vec<_> = command_name.split_whitespace().collect();
+    let mut command = command.unwrap_or_else(|| {
+        let mut command = Command::new(command_and_args[0]);
+        command.args(&command_and_args[1..]);
+        command
+    });
 
-    let mut cmd_to_run = Command::new(&command_parts[0]);
-    cmd_to_run
-        .args(&command_parts[1..])
-        .current_dir(chdir.as_ref());
-    Ok(run_command(
-        cmd_to_run,
-        command,
-        message_sender,
-        done_message_sender,
-        start_sender,
-        done_sender,
-    )?)
+    if let Some(cwd) = cwd {
+        command.current_dir(cwd);
+    }
+
+    let output = match spinner_vars {
+        Some(sv) => {
+            sv.status_message_sender.send(
+                sv.status_message_override
+                    .map(|s| s.as_ref().to_string())
+                    .unwrap_or_else(|| command_name.to_string()),
+            )?;
+            sv.done_message_sender.send(
+                sv.done_message_override
+                    .map(|s| s.as_ref().to_string())
+                    .unwrap_or_else(|| command_name.to_string()),
+            )?;
+            sv.activate_sender.send(())?;
+            let output = _run_internal(command, show_output)?;
+            sv.deactivate_sender.send(())?;
+            output
+        }
+        None => _run_internal(command, show_output)?,
+    };
+
+    Ok(output)
 }
 
-pub fn cd_and_run_no_spinner<S: AsRef<str>, P: AsRef<Path>>(
-    command: S,
-    chdir: P,
-) -> Result<Output, Box<dyn Error>> {
-    let command = command.as_ref();
-    let command_parts: Vec<_> = command.split_whitespace().collect();
+fn _run_internal(mut command: Command, show_output: bool) -> Result<Output, Box<dyn Error>> {
+    let mut child = command.spawn()?;
+    if show_output {
+        let out = child_stream_to_lines(child.stdout.take().unwrap());
+        let err = child_stream_to_lines(child.stderr.take().unwrap());
 
-    let mut cmd_to_run = Command::new(command_parts[0]);
-    cmd_to_run.args(&command_parts[1..]).current_dir(chdir);
-    Ok(run_command_no_spinner(cmd_to_run, command)?)
-}
-
-pub fn run<S>(
-    command: S,
-    message_sender: &Sender<String>,
-    done_message_sender: &Sender<String>,
-    start_sender: &Sender<()>,
-    done_sender: &Sender<()>,
-) -> Result<Output, Box<dyn Error>>
-where
-    S: AsRef<str>,
-{
-    let command = command.as_ref();
-    let command_parts: Vec<_> = command.split_whitespace().collect();
-
-    let mut cmd_to_run = Command::new(command_parts[0]);
-    cmd_to_run.args(&command_parts[1..]);
-    Ok(run_command(
-        cmd_to_run,
-        command,
-        message_sender,
-        done_message_sender,
-        start_sender,
-        done_sender,
-    )?)
-}
-
-pub fn run_no_spinner<S: AsRef<str>>(command: S) -> Result<Output, Box<dyn Error>> {
-    let command = command.as_ref();
-    let command_parts: Vec<_> = command.split_whitespace().collect();
-
-    let mut cmd_to_run = Command::new(command_parts[0]);
-    cmd_to_run.args(&command_parts[1..]);
-    Ok(run_command_no_spinner(cmd_to_run, command)?)
-}
-
-pub fn run_command<S: AsRef<str>>(
-    command: Command,
-    command_str: S,
-    message_sender: &Sender<String>,
-    done_message_sender: &Sender<String>,
-    start_sender: &Sender<()>,
-    done_sender: &Sender<()>,
-) -> Result<Output, Box<dyn Error>> {
-    println!(">> {}", command_str.as_ref());
-    let start_time = Utc::now();
-    message_sender.send(command_str.as_ref().to_string())?;
-    done_message_sender.send(command_str.as_ref().to_string())?;
-    start_sender.send(())?;
-    let result = run_with_status(command, done_sender, true)?;
-    let time_elapsed = Utc::now() - start_time;
-    println!(
-        "\x1b[2K\x1b[G> Time elapsed: {:?}",
-        time_elapsed.to_std().unwrap()
-    );
-    Ok(result)
-}
-
-pub fn run_command_no_spinner<S: AsRef<str>>(
-    mut command: Command,
-    command_str: S,
-) -> Result<Output, Box<dyn Error>> {
-    println!(">> {}", command_str.as_ref());
-    let start_time = Utc::now();
-    let child = command.spawn()?;
-    let result = child.wait_with_output()?;
-    let time_elapsed = Utc::now() - start_time;
-    println!(
-        "\x1b[2K\x1b[G> Time elapsed: {:?}",
-        time_elapsed.to_std().unwrap()
-    );
-    Ok(result)
+        while child.try_wait().is_err() {
+            let mut outl = out.lock();
+            let mut errl = err.lock();
+            if !outl.is_empty() || !errl.is_empty() {
+                let mut stdout = io::stdout();
+                stdout.write_all(b"\x1b[2K\x1b[G")?;
+                stdout.write_all(&outl)?;
+                stdout.write_all(&errl)?;
+                stdout.flush()?;
+            }
+            outl.clear();
+            errl.clear();
+        }
+    }
+    let output = child.wait_with_output()?;
+    Ok(output)
 }
 
 #[cfg(test)]
